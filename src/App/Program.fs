c1533fffdce49ec05ca21b4fb77ada0ef872ff15
MaxMEllon <maxmellon1994@gmail.com>
2019-10-08 01:32:02 +0900

    :tada: first commit

---
+++ src/App/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let PrintPrompt = fun () ->
+  printf "> "
+
+let rec forever f = 
+  f()
+  forever f
+
+[<EntryPoint>]
+let main =
+  forever <| fun () ->
+    PrintPrompt()
+    let line = Console.In.ReadLine()
+    printfn "%s" line
